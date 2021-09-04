79516b0c22074a967daefc63c8933abab1a2eee3
astro727 <88118613+astro727@users.noreply.github.com>
2021-09-04 13:38:48 -0400

    Add files via upload

---
+++ backendlog.php
@@ -0,0 +1,18 @@
+<?php
+
+$user = $_GET['user'];
+
+$password = $_GET['pass'];
+
+//echo $user;
+//echo $password;
+
+?>
+
+<!DOCTYPE html>
+
+<html>
+
+<h3 style="text-align: center">username: <?php echo $user?> password: <?php echo $password?></h3>
+</html>
+
